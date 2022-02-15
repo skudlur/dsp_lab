f20c3ab732da8734a6574d30087e81e9e19c1d8b
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2022-02-15 21:06:26 +0530

    made a few changes to the code

--- week2_circ_conv.m
+++ week2_circ_conv.m
@@ -1,5 +1,5 @@
 clc;
-clear all;
+clear;
 close all;
 
 x = input('x(n)=');
@@ -13,33 +13,38 @@ x = [x zeros(1, (N-l1))];
 h = [h zeros(1, (N-l2))];
 
 % using in-built function
-z = cconv(x,h,N);
-disp(z);
+%z = cconv(x,h,N);
+%disp(z);
 
 % using the formula 
 for n = 1:1:N
     y(n) = 0;
     for k = 1:1:N
-        y(n) = y(n) + x(k) .* h(mod(n-k), N);
+        y(n) = y(n) + x(k) * h(mod((n-k), N)+1);
     end
 end
 
+n = 0:(N-1);
 subplot(3,1,1);
-stem(n,x); grid on;
+stem(n,x,'k'); grid on;
+xlim([-1 4]);
 xlabel('Time');
 ylabel('Amplitude');
 title('Input sequence');
 
 subplot(3,1,2);
-stem(n,h); grid on;
+stem(n,h,'k'); grid on;
+xlim([-1 4]);
 xlabel('Time');
 ylabel('Amplitude');
 title('Impulse Response');
 
 subplot(3,1,3);
-stem(n,z); grid on;
+stem(n,y,'k'); grid on;
+xlim([-1 4]);
 xlabel('Time');
 ylabel('Amplitude');
-title('Output');
-disp(z);
+title('Circular Convolution');
+sgtitle('Suhas KV | PES2UG20EC093')
+disp(y);
 disp(n);
