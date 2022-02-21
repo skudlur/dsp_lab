3657b9e6b215a9dd13a509b3859c8cdef6b00b34
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2022-02-21 20:13:58 +0530

    added week 5 stuff

---
+++ w5_crossauto.m
@@ -0,0 +1,56 @@
+% Program to get cross correlation and auto correlation of 2 sequences
+
+clc;
+clear;
+close all;
+
+x = input('Enter the first sequence: ');
+y = input('Enter the second sequence: ');
+
+% fliplr --> to flip a sequence (reverse)
+
+l1 = length(x);
+l2 = length(y);
+
+L = l1 + l2 - 1;
+
+%% Formula for convolution
+% y(n) = (summation under k) x(k).h(n-k)
+
+%for n = 1 : L
+    %r(n) = 0;   
+    %for k = 1:l1
+        %if ((n-k)>=0 && (n-k)<=l2)
+            %r(n) = r(n)+r(k).y(n-k+1);
+        %end
+    %end
+%end
+
+%% Hermitian property
+
+r = xcorr(x,y);
+r1 = xcorr(y,x);
+if (r == fliplr(r1))
+    disp('Hermitian property is satisfied.');
+else
+    disp('Hermitian property is not satisfied.');
+end
+
+%% Covolution of the two sequences using in-built function]]
+lc = conv(x,y);
+R = int8(r); % To convert the data type
+disp(R);
+disp(lc);
+if (R == lc)
+    disp('Second sequence is symmetric.');
+else
+    disp('Second seqeunce is not symmetric.');
+end
+
+%% Orthogonality checking
+
+if r(((L-1)/2)+1) == 0
+    disp('Sequences are orthogonal.');
+else
+    disp('Sequences are not orthogonal.')
+end
