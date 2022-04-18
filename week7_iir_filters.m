2b8c6d7d2acf6bca599008026a2f701a04089609
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2022-04-18 21:35:14 +0530

    Add files via upload

---
+++ week7_iir_filters.m
@@ -0,0 +1,30 @@
+%% IIR filter
+% Butterworth filter
+
+clc;
+close all;
+clear;
+
+rp = input('Enter the pass band ripple (in dB): ');
+rs = input('Enter the stop band ripple (in dB): ');
+fs = input('Enter the sampling frequency (in Hz): ');
+fp = input('Enter the pass band frequency (in Hz): ');
+fst = input('Enter the stop band frequency (in Hz): ');
+
+fn = fs/2; % normalized sampling frequency
+fpn = fp/fn; % normalized pass band frequency
+fstn = fst/fn; % normalized stop band frequency
+
+[N, wc] = buttord(fpn, fstn, rp, rs);
+[b, a] = butter(N, wc);
+[H, f] = freqz(b, a, 256, fs);
+
+transferfunc = tf(b, a, (1/fs))
+H_mag = 20*log10(abs(H));
+figure;
+plot (f, H_mag);
+grid on;
+xlabel('Frequency in Hz');
+ylabel('Magnitude in dB');
+title('Magnitude Response');
+sgtitle('Suhas KV | PES2UG20EC093');
